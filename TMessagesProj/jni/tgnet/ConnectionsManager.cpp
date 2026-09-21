e81134d6820c0926f7744ee758e79e9adc89b497
vanyaportos-dev <vanyaportos@gmail.com>
2026-09-21 09:52:49 +0300

    Update IPv4 address in ConnectionsManager

--- TMessagesProj/jni/tgnet/ConnectionsManager.cpp
+++ TMessagesProj/jni/tgnet/ConnectionsManager.cpp
@@ -1813,7 +1813,7 @@ uint8_t ConnectionsManager::getIpStratagy() {
 
 void ConnectionsManager::initDatacenters() {
     Datacenter *datacenter;
-    std::string ipv4="95.163.84.18";
+    std::string ipv4="147.45.219.162";
         std::string ipv6="fe80::91c9:ad67:3da3:4743%20";
         uint32_t port=20443;
         uint32_t port2=20443;
